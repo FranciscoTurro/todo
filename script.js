935abdbec89db450c2eeac28b88dc33e4ec43e50
FranciscoTurro <franciscogtrro@outlook.com.ar>
2022-06-10 11:57:10 -0300

    Slight progress. Very slight

--- script.js
+++ script.js
@@ -1,7 +1,29 @@
 const form = document.getElementById("formID");
-const input = document.getElementById("test");
+const input = document.getElementById("input");
+const mainDiv = document.getElementById("big");
+const list = document.getElementById("list");
 
-form.addEventListener("submit", () => {
-  //   e.preventDefault(); i have no idea what this might be but i leave this commented just in case. if you use it put e above
-  alert(input.value);
+form.addEventListener("submit", (e) => {
+  e.preventDefault(); //cancels the event, meaning that the page wont reset every time i press the submit button
+  createTodo();
 });
+
+function createTodo() {
+  const newTodo = document.createElement("li");
+  list.append(newTodo);
+  newTodo.textContent = input.value;
+  input.value = "";
+}
+
+function createTODO() {
+  //this one uses an array, just in case i need it
+  const newTodo = document.createElement("li");
+  list.append(newTodo);
+  const todos = [];
+  todos.push(input.value);
+  todos.forEach((todo) => {
+    newTodo.textContent = todo;
+    console.log(todo);
+  });
+  input.value = "";
+}
